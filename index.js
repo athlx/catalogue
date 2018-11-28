9d9e90fb9c6ce83b21206aa74e287f6cf88374ef
MK Athlx <kode.athlx@gmail.com>
2018-11-28 21:18:48 +0400

    Display image number (out of total) and set "alt" attribute to the same

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 var slider = document.getElementById('slider');
+var imageNum = document.getElementById('num');
 var arrowNext = document.getElementById('next');
 var arrowPrev = document.getElementById('prev');
 
@@ -18,8 +19,12 @@ var images = [
 ];
 var imgIndex = 0;
 var slide = function(d) {
+  imgTotal = images.length;
   imgIndex += d;
   slider.src = images[imgIndex];
+  altText = 'Image ' + (imgIndex+1) + ' of ' + imgTotal;
+  slider.alt = altText;
+  imageNum.innerHTML = altText;
 }
 
 }
