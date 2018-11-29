12c25ac791ebdf25bf99ba5320b80cdac166ce15
MK Athlx <kode.athlx@gmail.com>
2018-11-30 01:13:29 +0400

    Move 'images' to separate file. It's now an array of objects.

---
+++ data.js
@@ -0,0 +1,19 @@
+var images = [
+	{
+		"url": "#",
+		"desc": "Samsung Galaxy Note 9",
+	},
+	{
+		"url": "#",
+		"desc": "iPhone XS Max",
+	},
+	{
+		"url": "#",
+		"desc": "iPad Pro",
+	},
+	{
+		"url": "#",
+		"desc": "Dell XPS 15",
+	},
+];
+
